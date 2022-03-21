92e3d49bc70b0dea9f9f53f99ebfef6dbcf386b7
miguelortis <pedroarguelles1996@gmail.com>
2022-03-20 23:29:35 -0430

    fix commit

--- model/DatingHistory.js
+++ model/DatingHistory.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const BeneficiarySchema = mongoose.Schema({
-  documentType: {
+const DatingHistorySchema = mongoose.Schema({
+  ref: {
     type: String,
     required: true,
   },
