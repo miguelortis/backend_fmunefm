65c30abaaaf244619635b9569c3cf1df17dd5d13
miguelortis <pedroarguelles1996@gmail.com>
2022-04-18 08:25:33 +0200

    fix

--- app.js
+++ app.js
@@ -362,7 +362,33 @@ io.on("connection", (socket) => {
   let nombre;
   console.log("usuario conectado");
 
-  socket.on("conectado", (socket) => {
-    console.log("usuario conec");
+  socket.on("connected", (hola) => {
+    console.log("usuario conec", hola);
   });
 });
+
+//Funcionalidad de socket.io en el servidor
+// io.on("connection", (socket) => {
+//   let nombre;
+
+//   socket.on("connect", (nomb) => {
+//     nombre = nomb;
+//     //socket.broadcast.emit manda el mensaje a todos los clientes excepto al que ha enviado el mensaje
+//     socket.broadcast.emit("mensajes", {
+//       nombre: nombre,
+//       mensaje: `${nombre} ha entrado en la sala del chat`,
+//     });
+//   });
+
+//   socket.on("mensaje", (nombre, mensaje) => {
+//     //io.emit manda el mensaje a todos los clientes conectados al chat
+//     io.emit("mensajes", { nombre, mensaje });
+//   });
+
+//   socket.on("disconnect", () => {
+//     io.emit("mensajes", {
+//       servidor: "Servidor",
+//       mensaje: `${nombre} ha abandonado la sala`,
+//     });
+//   });
+// });
